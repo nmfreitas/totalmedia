b3f238e6ec8aa865980f29c370ef96dbb0352d25
nmfreitas <nunomfreitas@proton.me>
2022-06-23 09:59:47 +0100

    fix typo

--- docker.sh
+++ docker.sh
@@ -17,7 +17,6 @@ sudo apt-get install  \
     curl \
     gnupg \
     lsb-release -y #install repositories
->>>>>>> testes
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg  | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
